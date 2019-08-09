a239f7195a04fda9708361520ffccfec9bb0dbd3
Steve <anil.stephen66@gmail.com>
2019-08-09 16:59:49 +0530

    Updated typeahead field

--- lib/form-fields/typeahead.php
+++ lib/form-fields/typeahead.php
@@ -1,3 +1,7 @@
+<?php
+	$places = array();
+	foreach ( $atts['items'] as $key => $value) { array_push( $places , $value['slug'] ); }
+?>
 <p>
-	<input type="text" data-behaviour="typeahead" name="<?php _e( $atts['form_name'] );?>" data-arr='<?php echo json_encode( $atts['items'] );?>' placeholder="<?php _e( $atts['placeholder'] );?>" value="<?php _e( $atts['form_value'] );?>" />
+	<input type="text" data-behaviour="typeahead" name="<?php _e( $atts['name'] );?>" data-arr='<?php echo json_encode( $places );?>' placeholder="<?php _e( $atts['placeholder'] );?>" value="<?php _e( $atts['value'] );?>" />
 </p>
