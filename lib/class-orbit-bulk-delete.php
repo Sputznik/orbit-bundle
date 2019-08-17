8bc9a821e74f0ebdc14065fddb8c1a08650401e5
root <vardhan.jay@gmail.com>
2019-08-17 18:28:36 +0530

    #59 added bulk delete support for terms

--- lib/class-orbit-bulk-delete.php
+++ lib/class-orbit-bulk-delete.php
@@ -6,21 +6,32 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 		/* ACTION HOOK FOR AJAX CALL - bulk delete */
 		add_action('orbit_batch_action_bulk_delete', function () {
 			
-			$per_page 	= $_GET['per_page'];
-			$post_type 	= $_GET['post_type'];
-			$batch_step = $_GET['orbit_batch_step'];
+			$per_page 		= $_GET['per_page'];
+			$batch_step 	= $_GET['orbit_batch_step'];
+			$resource_name 	= $_GET['resource_name'];
 
-			$this->handleBulkDelete($post_type, $per_page, $batch_step);
+
+			if( 'post_type' == $_GET['delete_type'] ) {
+
+				$this->handlePostsDelete($resource_name, $per_page, $batch_step);	
+			
+			} else if( 'taxonomy' == $_GET['delete_type'] ) {
+				
+				$this->handleTermsDelete($resource_name, $per_page, $batch_step);
+			}
+
+			
 		});
 	}
 
-	function handleBulkDelete( $post_type, $per_page, $batch_step ) {
+	function handlePostsDelete( $name, $per_page, $batch_step ) {
 
 		$posts= get_posts( array(
-			'post_type'=>$post_type,
-			'numberposts'=> $per_page 
+			'post_type'   => $name,
+			'numberposts' => $per_page 
 			) 
 		);
+		
 		
 		echo "<p><strong> Batch #". $batch_step ." </strong></p>";
 		
@@ -30,11 +41,35 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 			
 			wp_delete_post( $post->ID, true );
 			
-			echo "<li>Deleted Item id: ".$post->ID."</li>";
+			echo "<li>Deleted Post id: ".$post->ID."</li>";
 		}
 		
 		echo "</ul>";
 	
+
+	}
+
+
+	function handleTermsDelete( $name, $per_page, $batch_step ) {
+
+		$terms = get_terms( array(
+		    'taxonomy' => $name, 
+		    'hide_empty' => false,
+		) );
+
+
+		echo "<p><strong> Batch #". $batch_step ." </strong></p>";
+		
+		echo "<ul>";
+		
+		foreach ($terms as $term) {
+			
+			wp_delete_term( $term->term_id, $term->taxonomy );
+			
+			echo "<li>Deleted Term id: ".$term->term_id."</li>";
+		}
+		
+		echo "</ul>";
 
 	}
 
