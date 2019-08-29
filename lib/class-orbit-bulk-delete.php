ce68801298feb7c282e5d95933b669dc5a086403
unknown <samvthom16@gmail.com>
2019-08-29 16:28:07 +0530

    #59 - bulk delete to remove force deletion

--- lib/class-orbit-bulk-delete.php
+++ lib/class-orbit-bulk-delete.php
@@ -5,7 +5,7 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 	function __construct() {
 		/* ACTION HOOK FOR AJAX CALL - bulk delete */
 		add_action('orbit_batch_action_bulk_delete', function () {
-			
+
 			$per_page 		= $_GET['per_page'];
 			$batch_step 	= $_GET['orbit_batch_step'];
 			$resource_name 	= $_GET['resource_name'];
@@ -13,14 +13,14 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 
 			if( 'post_type' == $_GET['delete_type'] ) {
 
-				$this->handlePostsDelete($resource_name, $per_page, $batch_step);	
-			
+				$this->handlePostsDelete($resource_name, $per_page, $batch_step);
+
 			} else if( 'taxonomy' == $_GET['delete_type'] ) {
-				
+
 				$this->handleTermsDelete($resource_name, $per_page, $batch_step);
 			}
 
-			
+
 		});
 	}
 
@@ -28,24 +28,24 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 
 		$posts= get_posts( array(
 			'post_type'   => $name,
-			'numberposts' => $per_page 
-			) 
+			'numberposts' => $per_page
+			)
 		);
-		
-		
+
+
 		echo "<p><strong> Batch #". $batch_step ." </strong></p>";
-		
+
 		echo "<ul>";
-		
+
 		foreach ($posts as $post) {
-			
-			wp_delete_post( $post->ID, true );
-			
+
+			wp_delete_post( $post->ID );
+
 			echo "<li>Deleted Post id: ".$post->ID."</li>";
 		}
-		
+
 		echo "</ul>";
-	
+
 
 	}
 
@@ -53,22 +53,22 @@ class ORBIT_BULK_DELETE extends ORBIT_BASE {
 	function handleTermsDelete( $name, $per_page, $batch_step ) {
 
 		$terms = get_terms( array(
-		    'taxonomy' => $name, 
+		    'taxonomy' => $name,
 		    'hide_empty' => false,
 		) );
 
 
 		echo "<p><strong> Batch #". $batch_step ." </strong></p>";
-		
+
 		echo "<ul>";
-		
+
 		foreach ($terms as $term) {
-			
+
 			wp_delete_term( $term->term_id, $term->taxonomy );
-			
+
 			echo "<li>Deleted Term id: ".$term->term_id."</li>";
 		}
-		
+
 		echo "</ul>";
 
 	}
