bbef09a2fdedc8186b1a07480e363d1b124409b2
Steve <anil.stephen66@gmail.com>
2019-09-18 16:16:18 +0530

    #63 Added lazy loading on scroll

--- orbit-query/class-orbit-query-base.php
+++ orbit-query/class-orbit-query-base.php
@@ -1,39 +1,39 @@
 <?php
 
 class ORBIT_QUERY_BASE extends ORBIT_SHORTCODE{
-	
+
 	var $query;
 	var $shortcode;
-	
+
 	function __construct(){
-		
-		
+
+
 		add_action( 'wp_enqueue_scripts', array( $this, 'assets') );
-		
+
 		parent::__construct();
-		
-		
+
+
 	}
-	
+
 	// CHECK IF THE TEMPLATE FILE EXISTS IN THE THEME
 	function include_template_file( $template, $atts ){
-		
+
 		$old_template = $template;
-		
+
 		if( isset( $atts['style'] ) && $atts['style'] ){
 			$template = $template.'-'.$atts['style'];
 		}
-		
-		$template_url = $template.'.php';	
-		
+
+		$template_url = $template.'.php';
+
 		$theme_templates_url = apply_filters( 'orbit_query_template_'.$template , get_stylesheet_directory()."/orbit-query/".$template_url );
 		if( is_child_theme() && !file_exists( $theme_templates_url ) ){
 			// include from the parent theme
 			$theme_templates_url = apply_filters( 'orbit_query_template_'.$template , get_template_directory()."/orbit-query/".$template_url );
 		}
-		
+
 		$plugin_templates_url = plugin_dir_path(__FILE__)."templates/".$template_url;
-		
+
 		if( file_exists( $theme_templates_url ) ){
 			include( $theme_templates_url );
 		}
@@ -43,17 +43,17 @@ class ORBIT_QUERY_BASE extends ORBIT_SHORTCODE{
 		else{
 			include( "templates/".$old_template.".php" );
 		}
-		
+
 	}
-	
+
 	/* LOAD SCRIPTS AND STYLES IF THE SHORTCODE IS USED */
 	function assets($posts){
-		
+
 		$uri = plugin_dir_url( __FILE__ );
-			
+
 		// ENQUEUE SCRIPT
 		wp_enqueue_script('jquery');
-		wp_enqueue_script('oq-script', $uri.'js/orbit-query.js', array('jquery'), '1.0.1', true);
-			
+		wp_enqueue_script('oq-script', $uri.'js/orbit-query.js', array('jquery'), ORBIT_BUNDLE_VERSION, true);
+
 	}
 }
--- orbit-query/js/orbit-query.js
+++ orbit-query/js/orbit-query.js
@@ -1,103 +1,180 @@
 // CALLBACK FUNCTION AFTER THE DOM HAS BEEN LOADED
 jQuery('document').ready(function(){
-	
-	/* Lazy Loading of the List */
-    jQuery.fn.ajax_loading = function() {
-		
-        return this.each(function() {
 
-            var btn = jQuery(this);
-			
-			var ul = jQuery(btn.data('list'));
-			
-			
-			
-			
-			ul.sanitize_url = function() {
-                var url = ul.attr('data-url') ? ul.attr('data-url') : location.href;
-                var hash_index = url.indexOf('#');
-                if (hash_index > 0) {
-                    url = url.substring(0, hash_index);
-                }
-				
-                url = encodeURI(url);
-                
+	jQuery.fn.ajax_loading_list = function() {
+		return this.each(function() {
+
+			var $ul = jQuery( this );
+
+			function page_inc() {
+				var page = $ul.attr( 'data-page' ) ? parseInt( $ul.attr( 'data-page' ) ) : 1;
+				page += 1;
+				$ul.attr( 'data-page', page );
+				return page;
+			};
+
+			function sanitize_url() {
+        var url = $ul.attr('data-url') ? $ul.attr('data-url') : location.href,
+					hash_index = url.indexOf('#');
+
+				if (hash_index > 0) {
+          url = url.substring(0, hash_index);
+        }
+
+        url = encodeURI(url);
+
 				/* add page parameter to the request */
-                var page = ul.page_inc();
-				
-                var paged_attr = btn.attr('data-paged-attr') ? btn.attr('data-paged-attr') : 'paged';
+        var page = page_inc();
 
-                url += (url.split('?')[1] ? '&' : '?') + paged_attr + '=' + page;
-                return url;
-            };
+        var paged_attr = $ul.attr('data-paged-attr') ? $ul.attr('data-paged-attr') : 'paged';
 
-            ul.page_inc = function() {
-                var page = ul.attr('data-page') ? parseInt(ul.attr('data-page')) : 1;
-                page += 1;
-                ul.attr('data-page', page);
-                return page;
-            };
+        url += (url.split('?')[1] ? '&' : '?') + paged_attr + '=' + page;
+        return url;
+      };
 
-            ul.append_children = function(result) {
-				
-                if (jQuery(result).find(ul.attr('data-target')).length) {
-                    ul.attr('data-load-flag', '');
-                    btn.html( btn.data('html') )
-                } else {
-                    btn.hide();
-                }
-				
-				jQuery(result).find(ul.attr('data-target')).each(function() {
-                    var list = jQuery(this);
-                    list.hide();
-                    list.appendTo(ul);
-                    list.show('slow');
-                    list.trigger('sjax:init', [list]);
-                });
-            };
+			function append_children( result ) {
 
-            ul.ajax_load = function() {
+				if ( jQuery( result ).find( $ul.attr( 'data-target' ) ).length ) {
 
-                ul.attr('data-load-flag', 'ajax');
+					$ul.attr( 'data-load-flag', '' );
+					jQuery( result ).find( $ul.attr( 'data-target' ) ).each( function() {
+						var $list = jQuery(this);
+						$list.hide();
+						$list.appendTo( $ul );
+						$list.show('slow');
+						$list.trigger( 'sjax:init', [$list] );
+					});
+					$ul.trigger( 'ajax-load:complete' );
 
-                var url = ul.sanitize_url();
+				} else {
+					$ul.trigger( 'ajax-load:no-more' );
+				}
+			};
 
-               
-                jQuery.ajax({
-                    'url': url,
-                    'success': function(result) {
-                        //alert(result);
-                        console.log('lazy loading from sjax');
-                        ul.append_children(result);
-                    },
-                    'error': function() {
-                        console.log('lazy loading from cache');
+			function ajax(){
+
+				$ul.attr( 'data-load-flag', 'ajax' );
+
+				jQuery.ajax({
+					'url': sanitize_url(),
+					'success': function( result ) {
+						$ul.trigger( 'ajax-load:response' );
+						append_children( result );
+					},
+					'error': function() {
+						$ul.trigger( 'ajax-load:error' );
+						//console.log('lazy loading from cache');
 					}
-                });
+				});
+			};
 
-            };
-			
-            btn.click(function(ev) {
-				
-				btn.data('html', btn.html());
-				
-				btn.html('Loading ...');
-				
-                ul.ajax_load();
-            });
+			$ul.on( 'ajax-load:start', function( ev ){
 
-			
+				ajax();
 
-        });
+			} );
+
+		});
+	};
+
+	/* Lazy Loading of the List AT THE TRIGGER OF A BUTTON */
+    jQuery.fn.ajax_loading = function() {
+			return this.each(function() {
+
+				var $btn 			= jQuery( this ),
+					paged_attr 	= $btn.attr( 'data-paged-attr' ) ? $btn.attr( 'data-paged-attr' ) : 'paged',
+					$ul 				= jQuery( $btn.data( 'list' ) );
+
+				$ul.attr( 'data-paged-attr', paged_attr );
+
+				$ul.ajax_loading_list();
+
+				// Trigger load more on click
+				$btn.click( function( ev ) {
+					$btn.data( 'html', $btn.html() );
+					$btn.html( 'Loading ...' );
+					$ul.trigger( 'ajax-load:start' );
+				});
+
+				// Trigger load more on scroll
+				//var $btn_div = jQuery('[data-behaviour="oq-ajax-loading"]');
+				var $btn_bottom = $btn.offset().top + parseInt( $btn.height() ) + 60;
+
+				jQuery(window).scroll(function() {
+    			var window_position = jQuery(this).scrollTop() + parseInt( jQuery(window).height() );
+
+					// Test if the load button has reached
+					if( window_position > $btn_bottom && $btn.html() != "Loading ..." ){
+						$btn.data( 'html', $btn.html() );
+						$btn.html( 'Loading ...' );
+						$ul.trigger( 'ajax-load:start' );
+					}
+
+				});
+				// Trigger load more on scroll ends
+
+				$ul.on( 'ajax-load:complete', function( ev ){
+					$btn.html( $btn.data('html') );
+				} );
+
+				$ul.on( 'ajax-load:no-more', function( ev ){
+					$btn.hide();
+				} );
+
+				$ul.on( 'ajax-load:error', function( ev ){
+					$btn.hide();
+				} );
+
+			});
     };
     jQuery('body').find("[data-behaviour~=oq-ajax-loading]").ajax_loading();
-	
-	
-	
+
+	jQuery.fn.orbit_lazy_loading = function() {
+		return this.each(function() {
+			console.log( 'hi' );
+			var $el 				= jQuery( this ),
+				offset				= $el.data( 'offset' ) ? $el.data( 'offset' ) : 60,
+				loading_flag	= false,
+				$ul 					= jQuery( $el.data( 'list' ) );
+
+			$ul.ajax_loading_list();
+
+			// Trigger load more on scroll
+			var bottom = $el.offset().top + parseInt( $el.height() ) + offset;
+
+			jQuery(window).scroll(function() {
+
+				var window_position = jQuery(this).scrollTop() + parseInt( jQuery(window).height() );
+
+				// Test if the load button has reached
+				if( window_position > bottom && !loading_flag ){
+					$ul.trigger( 'ajax-load:start' );
+					loading_flag = true;
+				}
+
+			});
+			// Trigger load more on scroll ends
+			//
+			$ul.on( 'ajax-load:complete', function( ev ){
+				loading_flag = false;
+			} );
+
+			$ul.on( 'ajax-load:no-more', function( ev ){
+				$el.remove();
+			} );
+
+			$ul.on( 'ajax-load:error', function( ev ){
+				$el.remove();
+			} );
+
+		});
+	};
+	jQuery('body').find("[data-behaviour~=orbit-lazy-loading]").orbit_lazy_loading();
+
 	jQuery.fn.reload_html = function(){
         return this.each(function(){
 			var el = jQuery(this);
-            
+
             jQuery.ajax({
     			url : el.attr('data-url'),
         		success : function(result){
@@ -110,12 +187,12 @@ jQuery('document').ready(function(){
         			el.hide();
         		}
         	});
-			
-			
-			
-			
+
+
+
+
 		});
     };
     jQuery('body').find("[data-behaviour~=oq-reload-html]").reload_html();
-	
+
 });
