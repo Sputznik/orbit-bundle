73865ddcad401d43e7a2878bccfa85fdb29336f3
unknown <samvthom16@gmail.com>
2019-09-19 10:32:19 +0530

    #63 - document code

--- orbit-query/js/orbit-query.js
+++ orbit-query/js/orbit-query.js
@@ -6,6 +6,7 @@ jQuery('document').ready(function(){
 
 			var $ul = jQuery( this );
 
+			// GET THE PAGE INCREMENT VALUE AND INCREMENT IT BY 1
 			function page_inc() {
 				var page = $ul.attr( 'data-page' ) ? parseInt( $ul.attr( 'data-page' ) ) : 1;
 				page += 1;
@@ -13,6 +14,7 @@ jQuery('document').ready(function(){
 				return page;
 			};
 
+			// SANITIZE THE URL WITH PAGE INCREMENT VALUE
 			function sanitize_url() {
         var url = $ul.attr('data-url') ? $ul.attr('data-url') : location.href,
 					hash_index = url.indexOf('#');
@@ -32,6 +34,7 @@ jQuery('document').ready(function(){
         return url;
       };
 
+			// APPEND CHILDREN TO THE LIST USING THE LIST ITEM SELECTORS
 			function append_children( result ) {
 
 				if ( jQuery( result ).find( $ul.attr( 'data-target' ) ).length ) {
@@ -51,6 +54,7 @@ jQuery('document').ready(function(){
 				}
 			};
 
+			// AJAX REQUEST
 			function ajax(){
 
 				$ul.attr( 'data-load-flag', 'ajax' );
@@ -63,54 +67,51 @@ jQuery('document').ready(function(){
 					},
 					'error': function() {
 						$ul.trigger( 'ajax-load:error' );
-						//console.log('lazy loading from cache');
 					}
 				});
 			};
 
 			$ul.on( 'ajax-load:start', function( ev ){
-
 				ajax();
-
 			} );
 
 		});
 	};
 
 	/* Lazy Loading of the List AT THE TRIGGER OF A BUTTON */
-    jQuery.fn.ajax_loading = function() {
-			return this.each(function() {
+  jQuery.fn.ajax_loading = function() {
+		return this.each(function() {
 
-				var $btn 			= jQuery( this ),
-					paged_attr 	= $btn.attr( 'data-paged-attr' ) ? $btn.attr( 'data-paged-attr' ) : 'paged',
-					$ul 				= jQuery( $btn.data( 'list' ) );
+			var $btn 			= jQuery( this ),
+				paged_attr 	= $btn.attr( 'data-paged-attr' ) ? $btn.attr( 'data-paged-attr' ) : 'paged',
+				$ul 				= jQuery( $btn.data( 'list' ) );
 
-				$ul.attr( 'data-paged-attr', paged_attr );
+			$ul.attr( 'data-paged-attr', paged_attr );
 
-				$ul.ajax_loading_list();
+			$ul.ajax_loading_list();
 
-				// Trigger load more on click
-				$btn.click( function( ev ) {
-					$btn.data( 'html', $btn.html() );
-					$btn.html( 'Loading ...' );
-					$ul.trigger( 'ajax-load:start' );
-				});
-
-				$ul.on( 'ajax-load:complete', function( ev ){
-					$btn.html( $btn.data('html') );
-				} );
-
-				$ul.on( 'ajax-load:no-more', function( ev ){
-					$btn.hide();
-				} );
-
-				$ul.on( 'ajax-load:error', function( ev ){
-					$btn.hide();
-				} );
-
+			// Trigger load more on click
+			$btn.click( function( ev ) {
+				$btn.data( 'html', $btn.html() );
+				$btn.html( 'Loading ...' );
+				$ul.trigger( 'ajax-load:start' );
 			});
-    };
-    jQuery('body').find("[data-behaviour~=oq-ajax-loading]").ajax_loading();
+
+			$ul.on( 'ajax-load:complete', function( ev ){
+				$btn.html( $btn.data('html') );
+			} );
+
+			$ul.on( 'ajax-load:no-more', function( ev ){
+				$btn.hide();
+			} );
+
+			$ul.on( 'ajax-load:error', function( ev ){
+				$btn.hide();
+			} );
+
+		});
+  };
+  jQuery('body').find("[data-behaviour~=oq-ajax-loading]").ajax_loading();
 
 	jQuery.fn.orbit_lazy_loading = function() {
 		return this.each(function() {
@@ -155,27 +156,24 @@ jQuery('document').ready(function(){
 	jQuery('body').find("[data-behaviour~=orbit-lazy-loading]").orbit_lazy_loading();
 
 	jQuery.fn.reload_html = function(){
-        return this.each(function(){
+  	return this.each(function(){
+
 			var el = jQuery(this);
 
-            jQuery.ajax({
-    			url : el.attr('data-url'),
-        		success : function(result){
-        			//console.log(result);
-        			//console.log('reload html');
+      jQuery.ajax({
+    		url : el.attr('data-url'),
+        success : function(result){
+        	//console.log(result);
+        	//console.log('reload html');
 					el.html(result);
 					el.find("[data-behaviour~=oq-ajax-loading]").ajax_loading();
-            	},
-        		error : function(){
-        			el.hide();
-        		}
-        	});
-
-
-
-
+        },
+        error : function(){
+        	el.hide();
+        }
+      });
 		});
-    };
-    jQuery('body').find("[data-behaviour~=oq-reload-html]").reload_html();
+  };
+  jQuery('body').find("[data-behaviour~=oq-reload-html]").reload_html();
 
 });
