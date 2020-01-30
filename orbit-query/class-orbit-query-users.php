a6c1f1b086714eb384d75f75ffddccf1e5a2b098
unknown <samvthom16@gmail.com>
2020-01-30 16:33:55 +0530

    #69 - role__in atts in user query shortcode

--- orbit-query/class-orbit-query-users.php
+++ orbit-query/class-orbit-query-users.php
@@ -1,56 +1,58 @@
 <?php
 
 class ORBIT_QUERY_USERS extends ORBIT_QUERY_BASE{
-	
+
 	function __construct(){
-		
+
 		$this->shortcode = 'orbit_query_users';
-		
+
 		parent::__construct();
 	}
-	
-	function get_default_atts() {	
+
+	function get_default_atts() {
 		return array(
-			'search' 		=> '',
-			'role'			=> '',
+			'search' 				=> '',
+			'role'					=> '',
+			'role__in'			=> '',
 			'role__not_in'	=> '',
-			'exclude' 		=> '',
-			'include' 		=> '',
-			'orderby'		=> 'ID',
-			'order'			=> 'ASC',
-			'number'		=> '10',
-			'paged'			=> '1',
-			'style'			=> '',
-			'id'			=> 'users-'.rand()
+			'exclude' 			=> '',
+			'include' 			=> '',
+			'orderby'				=> 'ID',
+			'order'					=> 'ASC',
+			'number'				=> '10',
+			'paged'					=> '1',
+			'style'					=> '',
+			'id'						=> 'users-'.rand()
 		);
 	}
-	
+
 	function plain_shortcode( $atts, $content = false ){
 		ob_start();
 		$atts = $this->get_atts($atts);
-			
+
 		$query_atts = array(
-			'search' 		=> $atts['search'], 
-			'role'			=> $atts['role'],
+			'search' 				=> $atts['search'],
+			'role'					=> $atts['role'],
+			'role__in'			=> ! empty($atts['role__in']) ? explode(',', $atts['role__in']) : '',
 			'role__not_in'	=> ! empty($atts['role__not_in']) ? explode(',', $atts['role__not_in']) : '',
-			'exclude' 		=> ! empty($atts['exclude']) ? explode(',', $atts['exclude']) : '',
-			'include' 		=> ! empty($atts['include']) ? explode(',', $atts['include']) : '',
-			'orderby'		=> $atts['orderby'],
-			'order'			=> $atts['order'],
-			'number'		=> $atts['number'],
-			'paged'			=> $atts['paged'],
+			'exclude' 			=> ! empty($atts['exclude']) ? explode(',', $atts['exclude']) : '',
+			'include' 			=> ! empty($atts['include']) ? explode(',', $atts['include']) : '',
+			'orderby'				=> $atts['orderby'],
+			'order'					=> $atts['order'],
+			'number'				=> $atts['number'],
+			'paged'					=> $atts['paged'],
 		);
-		
+
 		$this->query = new WP_User_Query( $query_atts );
-		
+
 		if( ! empty( $this->query->results ) ){
 			the_oq_users( $atts );
 		}
-			
+
 		return ob_get_clean();
 	}
-	
+
 }
 
-global $orbit_query_users;	
+global $orbit_query_users;
 $orbit_query_users = new ORBIT_QUERY_USERS;
