431041cff9349717f8f59201a28c8651f0b520bd
psychome004 <anil.stephen66@gmail.com>
2023-09-29 11:23:48 +0530

    #95 - Add pagination in orbit-query-users shortcode

--- orbit-query/class-orbit-query-users.php
+++ orbit-query/class-orbit-query-users.php
@@ -20,10 +20,27 @@ class ORBIT_QUERY_USERS extends ORBIT_QUERY_BASE{
 			'orderby'				=> 'ID',
 			'order'					=> 'ASC',
 			'number'				=> '10',
+			'offset'				=> '0',
+			'pagination'		=> '0',
 			'paged'					=> '1',
 			'style'					=> '',
 			'id'						=> 'users-'.rand()
 		);
+	}
+
+	/* OVERRIDDEN FROM THE PARENT */
+	function get_ajax_url( $atts ){
+		return $this->get_ajax_url_from_args( $atts, array('paged') );
+	}
+
+	function get_offset( $atts ){
+
+		// IF PAGED ATTRIBUTE IS PASSED AS A GET PARAM
+		if( get_query_var('paged') ){
+			$atts['paged'] = max( 1, get_query_var('paged') );
+		}
+
+		return (((int)$atts['paged'] - 1) * (int)$atts['number']) + (int)$atts['offset'];
 	}
 
 	function plain_shortcode( $atts, $content = false ){
@@ -39,14 +56,27 @@ class ORBIT_QUERY_USERS extends ORBIT_QUERY_BASE{
 			'include' 			=> ! empty($atts['include']) ? explode(',', $atts['include']) : '',
 			'orderby'				=> $atts['orderby'],
 			'order'					=> $atts['order'],
-			'number'				=> $atts['number'],
-			'paged'					=> $atts['paged'],
+			'number'				=> (int) $atts['number'],
+			'offset'				=> self::get_offset($atts)
 		);
+
+		/* DONT FETCH SQL_CALC_FOUND_ROWS */
+		if( $atts['pagination'] == '0' ){
+			$query_atts['count_total'] = false;
+		}
 
 		$this->query = new WP_User_Query( $query_atts );
 
+		// CALCULATE TOTAL NUMBER OF PAGES BASED ON THE QUERY
+		$total_pages = $this->query->total_users && $query_atts['number'] > 0 ? ceil( $this->query->total_users / $query_atts['number'] ) : 0;
+
 		if( ! empty( $this->query->results ) ){
 			the_oq_users( $atts );
+
+			// SHOW LOAD MORE
+			if( $total_pages > 1 ){
+				the_oq_pagination( $atts );
+			}
 		}
 
 		return ob_get_clean();
