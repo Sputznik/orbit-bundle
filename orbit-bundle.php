8d129177f991dc0a427f7f5cab82d89c5e363907
Steve <anil.stephen66@gmail.com>
2025-12-09 12:36:44 +0530

    Update version number

--- orbit-bundle.php
+++ orbit-bundle.php
@@ -4,12 +4,12 @@
     Plugin URI: http://sputznik.com
     Description: Create Wordpress Custom post types and custom taxonomies. Search and filter through the wordpress post types and create custom queries using simple shortcodes.
     Author: Samuel Thomas
-    Version: 2.2.0
+    Version: 2.3.0
     Author URI: http://sputznik.com
     */
 
 
-	define( 'ORBIT_BUNDLE_VERSION', '2.2.0' );
+	define( 'ORBIT_BUNDLE_VERSION', '2.3.0' );
 
 	$inc_files = array(
 		"lib/class-orbit-base.php",
